8fe68bb1b36b92bb17e679a7a9443457d73d87d8
PIUS <relimoringa@gmail.com>
2023-06-15 20:46:09 +0300

    complete

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let response;
+  
+  if (feet <= 400) {
+    response = "This one is on me!";
+  } else if (feet > 2000 && feet <= 2500) {
+    response = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    response = "No can do.";
+  }
+  
+  return response;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+  }
+
+  return response;
 }
